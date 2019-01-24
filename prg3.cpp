9f8032f66ce4b9935796ba65d3bee7373d2c2043
Nil Sinojiya <nil.sinojiya@yahoo.com>
2019-01-24 23:53:28 +0530

    prg3-4

--- prg3.cpp
+++ prg3.cpp
@@ -1,1 +1,16 @@
 //Write a cpp program to accept the number and display it square.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+        cout<<"enter a number:";
+        cin>>num;
+
+        cout<<"Square of given number is:"<<(num*num);
+
+        return 0;
+}
--- prg4.cpp
+++ prg4.cpp
@@ -1,1 +1,13 @@
 //Write a cpp program to display multiplication of two numbers.
+
+
+int main()
+{
+        int a,b;
+
+            cout<<"Enter two number:";
+            cout<<"\na="; cin>>a;
+            cout<<"\nb="; cin>>b;
+
+            cout<<"\nMultiplication of given number is:"<<a*b;
+}
