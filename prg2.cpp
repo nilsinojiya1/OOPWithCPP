a35a9e78d4cb2e0aca2ed0929551865cdde86cba
Nil Sinojiya <nil.sinojiya@yahoo.com>
2019-01-24 23:34:47 +0530

    Update prg2.cpp

--- prg2.cpp
+++ prg2.cpp
@@ -1,1 +1,38 @@
 //Write a cpp program enter the 3 numbers and find largest number
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"enter a three number"<<endl;
+    cout<<"a=";
+    cin>>a;
+    cout<<"\nb=";
+    cin>>b;
+    cout<<"\nc=";
+    cin>>c;
+    if(a>b)
+    {
+        if(a>c)
+        {
+            cout<<"c is greater.";
+        }
+        else
+        {
+            cout<<"c is greater.";
+        }
+    }
+    else
+    {
+        if(b>c)
+        {
+            cout<<"b is greater.";
+        }
+        else
+        {
+            cout<<"c is greater.";
+        }
+    }
+}
