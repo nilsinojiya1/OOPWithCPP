2f267dfad8d4292a142d12c4ab294ff26d93fa9a
Nil Sinojiya <nil.sinojiya@yahoo.com>
2019-02-23 12:28:21 +0530

    complete

--- prg25.cpp
+++ prg25.cpp
@@ -1,1 +1,24 @@
-#Write a cpp program to convert basic type to class type.
+//Write a cpp program to convert basic type to class type.
+#include<iostream>
+using namespace std;
+class sample{
+private:
+    int a;
+public:
+    sample(){}
+    sample(int x){
+        a=x;
+    }
+void display(){
+    cout<<"the value of 'a':"<<a;
+    }
+};
+int main(){
+int m;
+cout<<"\nenter a value of 'a':";
+cin>>m;
+sample s;
+s=m;
+s.display();
+return 0;
+}
--- prg27.cpp
+++ prg27.cpp
@@ -1,1 +1,65 @@
-#Write a cpp program to convert class type to class type.
+//Write a cpp program to convert class type to class type.
+#include<iostream>
+using namespace std;
+class stock2;
+class stock1{
+private:
+    int code,item;
+    float price;
+public:
+    stock1(int a,int b,float c){
+    code=a;
+    item=b;
+    price=c;
+    }
+    void display(){
+        cout<<"\ncode:"<<code;
+        cout<<"\nitem"<<item;
+        cout<<"\nprince per items:"<<price;
+        }
+    int getcode(){
+        return code;
+        }
+    int getitem(){
+        return item;
+        }
+    int getprice(){
+        return price;
+        }
+    operator float(){
+        return (item * price);
+        }
+    };
+class stock2{
+private:
+    int code;
+    float val;
+public:
+    stock2(){
+    code=0;
+    val=0;
+    }
+    stock2(int x, int y){
+    code=x;
+    val=y;
+    }
+    void display(){
+    cout<<"\ncode:"<<code;
+    cout<<"\ntotal value RS."<<val;
+    }
+    stock2(stock1 p){
+    code=p.getcode();
+    val=p.getitem() * p.getprice();
+    }
+};
+int main(){
+    stock1 i1(101,10,125.0);
+    stock2 i2;
+    float totVal=i1;
+    i2=i1;
+    cout<<"stock detail: stock1 type";
+    i1.display();
+    cout<<"stock detail: stock type";
+    i2.display();
+    return 0;
+}
