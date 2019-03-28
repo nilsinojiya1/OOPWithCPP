a6e8194474c38360a1951173fb527c6575e97b5c
Nil Sinojiya <nil.sinojiya@yahoo.com>
2019-03-28 23:19:57 +0530

    Update prg18.cpp

--- prg18.cpp
+++ prg18.cpp
@@ -1,1 +1,15 @@
 //Write a cpp program to use default constructor.
+#include<iostream>
+using namespace std;
+class A{
+public:
+    A()
+    {
+        cout<<"default constructor called.";
+    }
+};
+int main()
+{
+    A x;
+    return 0;
+}
