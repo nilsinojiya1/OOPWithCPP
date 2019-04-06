8acf0c340464a76417dd31e39ba851951e9fd240
Nil Sinojiya <nil.sinojiya@yahoo.com>
2019-04-06 23:15:13 +0530

    Update prg26.cpp
    with code

--- prg26.cpp
+++ prg26.cpp
@@ -1,1 +1,28 @@
-#Write a cpp program to convert class type to basic type.
+//Write a cpp program to convert class type to basic type.
+#include<iostream>
+using namespace std;
+
+class vector{
+   int a[5];
+   public:
+   vector(){
+	for(int i=0;i<5;i++)
+     		a[i] = i*2;
+   }
+   operator int();
+};
+vector:: operator int() {
+   int sum=0;
+   for(int i=0;i<5;i++)
+   	sum = sum + a[i];
+   return sum;
+}
+int main()
+   {
+   vector v;
+   int len;
+   len = v;
+   cout<<"Length of V="<<len;
+   return 0;
+   }
+
