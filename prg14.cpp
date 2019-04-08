854f0e97bc1461fe6e552e836ea1a06d03732542
Nil Sinojiya <31802494+nilsinojiya1@users.noreply.github.com>
2019-04-08 17:34:28 +0530

    Update prg14.cpp

--- prg14.cpp
+++ prg14.cpp
@@ -1,1 +1,31 @@
 //Write a cpp program for static member functions.
+#include<iostream>
+using namespace std;
+class item
+{
+   int number;
+   static int count;
+   public:
+   void getdata(int a){
+     number = a;
+     count++;
+   }
+   static void getcount(){
+     cout<<"\nvalue of count: "<<count;
+   }
+};
+int item :: count;
+
+int main()
+{
+    item a,b,c;
+    a.getdata(100);
+    item::getcount();
+
+    b.getdata(200);
+    item::getcount();
+
+    c.getdata(300);
+    item::getcount();
+    return 0;
+}
