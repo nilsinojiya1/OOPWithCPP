0731328e1493ddb42087bd2bf4ea2b6d7c7e2b60
Nil Sinojiya <31802494+nilsinojiya1@users.noreply.github.com>
2019-04-08 17:33:16 +0530

    Update prg11.cpp

--- prg11.cpp
+++ prg11.cpp
@@ -1,1 +1,16 @@
 //Write a cpp program for find area of rectangle using inline function.
+#include<iostream>
+using namespace std;
+
+inline float area(float x,float y)
+{
+    cout<<"area="<<x*y;
+}
+int main()
+{
+    float x,y;
+    cout<<"enter length:"; cin>>x;
+    cout<<"\nenter breadth:"; cin>>y;
+    area(x,y);
+    return 0;
+}
