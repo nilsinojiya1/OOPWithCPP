bdb211e14762c2043d578d5586cde580153684f4
Nil Sinojiya <31802494+nilsinojiya1@users.noreply.github.com>
2019-04-08 17:36:55 +0530

    Update prg6.cpp

--- prg6.cpp
+++ prg6.cpp
@@ -1,1 +1,14 @@
 //Write a cpp program to display word computer 5 times using loop.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cout<<"computer\n";
+    }
+    return 0;
+}
