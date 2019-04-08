d29f2587578c7400cc85cb5e92ac48ee2b39334e
Nil Sinojiya <31802494+nilsinojiya1@users.noreply.github.com>
2019-04-08 17:36:33 +0530

    Update prg5.cpp

--- prg5.cpp
+++ prg5.cpp
@@ -1,1 +1,18 @@
 //Write a cpp program to calculate simple interest taking principale and rate of interest and no of uses.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float rate,amount,time;
+
+    cout<<"enter total amount:";
+    cin>>amount;
+    cout<<"\nenter rate of interest:";
+    cin>>rate;
+    cout<<"enter time:";
+    cin>>time;
+
+    cout<<"interest="<<(1+(rate*time/100))*(amount);
+}
