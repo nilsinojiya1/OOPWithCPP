5cbb1b9851251222bd6da099d1fa5ff6ed2a0750
Nil Sinojiya <31802494+nilsinojiya1@users.noreply.github.com>
2019-04-08 17:32:50 +0530

    Update prg10.cpp

--- prg10.cpp
+++ prg10.cpp
@@ -1,1 +1,13 @@
 //Write a cpp program for reference variable.
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int &b=a;
+    cout<<a;
+    cout<<"\n"<<b;
+    return 0;
+}
