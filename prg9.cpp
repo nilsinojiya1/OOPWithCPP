47d6ba386ae676ce3cd163933aacebb2ba41bccd
Nil Sinojiya <31802494+nilsinojiya1@users.noreply.github.com>
2019-04-08 17:38:04 +0530

    Update prg9.cpp

--- prg9.cpp
+++ prg9.cpp
@@ -1,1 +1,14 @@
 //Write a cpp program for scope resolution operator.
+#include<iostream>
+
+using namespace std;
+
+int a=10;
+
+int main()
+{
+    int a=5;
+    cout<<"a="<<a;
+    cout<<"\n::a"<<::a;
+    return 0;
+}
